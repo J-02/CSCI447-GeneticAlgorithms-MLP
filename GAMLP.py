47ce0c701455eb181096e418533e01d707eda422
Mark Braun <markrbraun19@gmail.com>
2022-11-26 17:54:05 -0700

    Implemented crossover/mutation, removed extraneous code, mutation and crossover should be working but suboptimal, haven't set weights yet

--- GAMLP.py
+++ GAMLP.py
@@ -4,161 +4,6 @@ from MLP import MLP
 import DataSpilt as ds
 import random
 
-
-# GA Experimentation Pseudocode; Trains a population of MLPs and uses a GA to select the best one
-
-class GAModel:
-    # initializes with a population of solutions, a probability of crossover, a probability of mutation, and a mutation SD
-    def __init__(self, data, nodes, step_size=.001, momentum=.5, solutionPopulationWeights=[], probOfCrossover=.1,
-                 probOfMutation=.05, mutationSD=.01):
-        self.solutionPopulation = solutionPopulationWeights
-        self.populationSize = len(solutionPopulationWeights)
-        self.populationFitnesses = []
-        self.probOfCrossover = probOfCrossover
-        self.probOfMutation = probOfMutation
-        self.mutationSD = mutationSD
-        self.data = data
-        self.nodes = nodes
-        self.step_size = step_size
-        self.momentum = momentum
-        for model in self.solutionPopulation:
-            # calculate fitnesses of all models in the solution population
-            MLP_model = MLP(self.data, self.nodes, None, self.step_size, self.momentum)
-            self.populationFitnesses.append(MLP_model.Train())
-
-    # training function, iterates through generations until the best model in a given generation doesn't improve sufficiently
-    # from previous generation
-    def Train(self):
-        converged = False
-        lastBestFitness = 0
-        generations = 0
-        timesWithoutChange = 0
-        while not converged:
-            print('Generation', generations + 1)
-            newSolutionPopulation = []
-            newPopulationFitnesses = []
-            # performs n/5 reproductions, will probably need to figure out how to decide this later
-            for i in range(int(self.populationSize / 5)):
-                # easier to return the indices in the list of the parents selected
-                parent1, parent2 = self.selectParents()
-
-                # crossover the 2 selected parents
-                child1, child2 = self.crossParents(self.solutionPopulation[parent1], self.solutionPopulation[parent2])
-
-                # randomly mutate some genes in each child
-                child1 = self.Mutate(child1)
-                child2 = self.Mutate(child2)
-
-                # using generational replacement, can adjust to steady state later if we want
-
-                # remove parents from original population so not selected multiple times (should they be able to be
-                # selected multiple times?)
-                parent_indices = [parent1, parent2]
-                self.solutionPopulation = [i for j, i in enumerate(self.solutionPopulation) if j not in parent_indices]
-                self.populationFitnesses = [i for j, i in enumerate(self.solutionPopulation) if j not in parent_indices]
-
-                # add children to the "next generation"
-                newSolutionPopulation.append(child1)
-                newSolutionPopulation.append(child2)
-
-                # add children performances to the "next generation"
-                newPopulationFitnesses.append(MLP(self.data, self.nodes, child1, self.step_size, self.momentum).Train())
-                newPopulationFitnesses.append(MLP(self.data, self.nodes, child2, self.step_size, self.momentum).Train())
-
-            # set the current generation population to the newly generated population of children
-            self.solutionPopulation.append(newSolutionPopulation)
-            self.populationFitnesses.append(newPopulationFitnesses)
-
-            # continue until the best solution in the children population doesn't improve that much, and return
-            # that solution
-            maxSolutionFitness = max(self.populationFitnesses)
-            bestSolution = self.solutionPopulation[np.argmax(self.populationFitnesses)]
-
-            # convergence conditions: if 50 generations have passed, or if 10 generations in a row have passed without
-            # a .1% change in best population fitness, converge
-            if generations == 50: converged = True
-            if abs(maxSolutionFitness - lastBestFitness) < .001 * lastBestFitness:
-                timesWithoutChange = timesWithoutChange + 1
-                if timesWithoutChange == 10: converged = True
-            else:
-                timesWithoutChange = 0
-            lastBestFitness = maxSolutionFitness
-            print('This generation best fitness: ', maxSolutionFitness)
-
-        return bestSolution, maxSolutionFitness
-
-    # select 2 parents to reproduce and create 2 children
-    def selectParents(self):
-        # fitness proportionate
-        # sum up all fitnesses across entire solution population
-        fitnessSum = np.sum(self.populationFitnesses)
-        # calcuate probabilities of each solution being selected for reproduction
-        selectionProbabilities = self.populationFitnesses / fitnessSum
-
-        # rank proportionate
-        # sum up all ranks across entire solution population
-        # rankSum = np.sum(range(self.populationSize+1))
-        # find ranks of all models in population
-        # sortedRanks = np.argsort(self.populationFitnesses)
-        # define selection probabilities by these ranks
-        # selectionProbabilities = sortedRanks+1/rankSum
-
-        # choosing 2 parents based on probabilities
-        parent1, parent2 = np.random.choice(range(self.populationSize), size=2, p=selectionProbabilities)
-        return parent1, parent2
-
-    # cross parents over, uniform crossover (with 10% probability) implemented but could switch to one point or two point
-    def crossParents(self, parent1, parent2):
-
-        # need to flatten weight matrices into 1d arrays to get into "chromosome" form
-        # not sure syntactically the best way to do this, can think of some shitty ways though
-        parentChromosome1 = parent1
-        parentChromosome2 = parent2
-
-        childChromosome1 = parentChromosome1
-        childChromosome2 = parentChromosome2
-
-        # uniform crossover
-        # swap the genes from the two parents in the children with some fixed probability
-        # iterate through all genes in the chromosomes
-        crossoverBinaries = np.random.choice([0, 1], p=[1 - self.probOfCrossover, self.probOfCrossover],
-                                             size=len(parentChromosome1))
-        childChromosome1 = np.choose(crossoverBinaries, [parentChromosome1, parentChromosome2])
-        childChromosome2 = np.choose(crossoverBinaries, [parentChromosome2, parentChromosome1])
-
-        # one point crossover
-        # crossoverPoint = random.randint(0, len(parentChromosome1))
-        # childChromosome1[0:crossoverPoint] = parentChromosome2[0:crossoverPoint]
-        # childChromosome2[crossoverPoint+1:len(parentChromosome1)-1] = parentChromosome1[crossoverPoint+1:len(parentChromosome1)-1]
-
-        # two point crossover
-        # point1 = random.randint(0, len(parentChromosome1))
-        # point2 = random.randint(0, len(parentChromosome1))
-        # childChromosome1[min(point1, point2):max(point1, point2)] = parentChromosome2[min(point1, point2):max(point1, point2)]
-        # childChromosome2[min(point1, point2):max(point1, point2)] = parentChromosome1[min(point1, point2):max(point1, point2)]
-
-        # return children as new solutions with child chromosomes as weight matrix, all else held the same
-        child1 = childChromosome1
-        child2 = childChromosome2
-        return child1, child2
-
-    # randomly alter some genes in a given solution's chromosome, with fixed probability
-    def Mutate(self, solution):
-
-        # again, probably an optimal way to get to this 1d array, or might not even be necessary
-        chromosome = solution
-
-        # iterate through all genes, mutate some % with a term from a normal distribution with fixed SD (need to tune)
-        mutationBinaries = np.random.choice([0, 1], p=[1 - self.probOfMutation, self.probOfMutation],
-                                            size=len(chromosome))
-        mutationTerms = np.random.normal(0, self.mutationSD, size=len(chromosome))
-        chromosome = chromosome + mutationBinaries * mutationTerms
-
-        mutatedSolution = chromosome
-
-        return mutatedSolution
-
-
 class Dataset:
     def __init__(self, data):
         if ".data" in data:
@@ -258,46 +103,73 @@ class Network:
     # performs genetic algorithm
     def geneticAlgorithm(self, prob=.05, SD=.01):
 
-        def select(array, x = 10):
+        def select(fitnesses, x = 5):
             # should select x number of pairs weighting selection odds by fitness
-            pSelection = array / np.sum(array)
-            pairs = [np.random.choice(np.where(array)[0], p=pSelection, replace=False, size=2) for i in range(x)]
+            pSelection = fitnesses / np.sum(fitnesses)
+            pairs = [np.random.choice(np.where(fitnesses)[0], p=pSelection, replace=False, size=2) for i in range(x)]
             return pairs
 
         def crossover(pairs):
 
-            # todo: crossover pairs from select method, currently just randomly selects
-            xover = np.random.choice([0, 1], p=[0.5, 0.5], size=(array.shape[0] // 2, array.shape[1], array.shape[2]))
-            xover2 = (~xover.astype(bool)).astype(int)
-            children1 = np.array([np.choose(xover[i], array[i::5]) for i in range(5)])
-            children2 = np.array([np.choose(xover2[i], array[i::5]) for i in range(5)])
-            children = np.vstack([children1, children2])
-            return children
+            nextGenW = []
+            nextGenB = []
+            #iterate through each pair in the selected set of pairs
+            for pair in pairs:
+                parent1W = [i[pair[0],:,:] for i in self.weights]
+                parent1B = [i[0,pair[0],:] for i in self.bweights]
 
+                parent2W = [i[pair[1], :, :] for i in self.weights]
+                parent2B = [i[0,pair[1],:] for i in self.bweights]
+
+                #randomly select genes that will be crossed over between the two parents
+                xoverW = [np.random.choice([0,1], p=[.5, .5], size=i.shape) for i in parent1W]
+                xoverB = [np.random.choice([0,1], p=[.5, .5], size=i.shape) for i in parent1B]
+
+                child1W = parent1W
+                child1B = parent1B
+                child2W = parent2W
+                child2B = parent2B
+
+                #crossover the two children at the selected indices
+                for i in range(len(child1W)):
+                    child1W[i][np.where(xoverW[i] == 1)] = parent2W[i][np.where(xoverW[i] == 1)]
+                    child2W[i][np.where(xoverW[i] == 1)] = parent1W[i][np.where(xoverW[i] == 1)]
+                    child1B[i][np.where(xoverB[i] == 1)] = parent2B[i][np.where(xoverB[i] == 1)]
+                    child2B[i][np.where(xoverB[i] == 1)] = parent1B[i][np.where(xoverB[i] == 1)]
+
+                child1W = Mutate(child1W, .05, .01)
+                child1B = Mutate(child1B, .05, .01)
+                child2W = Mutate(child2W, .05, .01)
+                child2B = Mutate(child2B, .05, .01)
+                nextGenW.append(child1W)
+                nextGenW.append(child2W)
+                nextGenB.append(child1B)
+                nextGenB.append(child2B)
+            pass
         # randomly alter some genes in a given solution's chromosome, with fixed probability
-        def Mutate(children):
-            # iterate through all genes, mutate some % with a term from a normal distribution with fixed SD (need to tune)
-            mutationBinaries = np.random.choice([0, 1], p=[1 - prob, prob], size=children.size())
-            mutationTerms = np.random.normal(0, SD, size=children)
-            mutatedChildren = children + mutationBinaries * mutationTerms
+        def Mutate(child, mutationProb, mutationSD):
+            mutationBinaries = [np.random.choice([0,1], p=[1-mutationProb, mutationProb], size=i.shape) for i in child]
+            mutationTerms = [np.random.normal(0, mutationSD, size=i.shape) for i in child]
+            mutatedChild = child
+            for i in range(len(child)):
+                mutatedChild[i] = child[i] + mutationBinaries[i] * mutationTerms[i]
+            return mutatedChild
 
-            return mutatedChildren
-
-        # crosses over then mutates weights
-        def evolve():
-            self.weights = [Mutate(crossover(i)) for i in self.weights]
-            self.bweights = [Mutate(crossover(i)) for i in self.bweights]
         def run():
 
             # evaluate weights and assign performance
             fitness = self.evaluate()
-            pairs = select(fitness)
+            pairs = select(fitness, int(len(fitness)/2))
+            crossover(pairs)
 
-            pass
+
+            return np.max(fitness)
 
         done = False
+        lastGenBestFitness = np.max(self.evaluate())
         while not done:
-            run()
+            bestGenFitness = run()
+            if abs(lastGenBestFitness - bestGenFitness) < .01*lastGenBestFitness: done = True
 
     def diffEvo(self, xoP):
             # xoP = crossover probabiliy
